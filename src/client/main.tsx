6a6305f3f254afbc3d8afa377be41928171619cb
周新凯 <zhouxinkai@meituan.com>
2018-05-16 00:40:56 +0800

    fix bug

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,34 +1,34 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+// import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
 
-import { observer, Provider } from 'mobx-react';
+// import { observer, Provider } from 'mobx-react';
 
-import store from './stores';
-import Routes from './routes';
+// import store from './stores';
+// import Routes from './routes';
 
-import './styles/main.scss';
-import Alert from './components/Dialog/Alert';
+// import './styles/main.scss';
+// import Alert from './components/Dialog/Alert';
 
-@observer
-export default class App extends React.Component {
-  render() {
-    return (
-      <Provider {...store}>
-        <div className="main" id="main">
-          <Alert
-            show={store.errorStore.error !== null}
-            confirmText="知道了"
-            noCancel={true}
-            noClose={true}
-            onClosed={() => store.errorStore.setError(null)}
-          >
-            {store.errorStore.error && store.errorStore.error.message}
-          </Alert>
-          <Routes />
-        </div>
-      </Provider>
-    );
-  }
-}
+// @observer
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <Provider {...store}>
+//         <div className="main" id="main">
+//           <Alert
+//             show={store.errorStore.error !== null}
+//             confirmText="知道了"
+//             noCancel={true}
+//             noClose={true}
+//             onClosed={() => store.errorStore.setError(null)}
+//           >
+//             {store.errorStore.error && store.errorStore.error.message}
+//           </Alert>
+//           <Routes />
+//         </div>
+//       </Provider>
+//     );
+//   }
+// }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// ReactDOM.render(<App />, document.getElementById('app'));
