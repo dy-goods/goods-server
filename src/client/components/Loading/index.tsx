f6d4af864cc29fad003bb0c8a504347386884666
周新凯 <zhouxinkai@meituan.com>
2018-05-18 00:14:05 +0800

    UPDATE loading component

--- src/client/components/Loading/index.tsx
+++ src/client/components/Loading/index.tsx
@@ -2,15 +2,42 @@ import * as React from 'react';
 
 import './index.scss';
 
-export default class Loading extends React.Component {
+type IPros = {
+  showFlag?: 'circle' | 'spinner' | 'test';
+};
+
+export default class Loading extends React.Component<IPros> {
   constructor(props: any) {
     super(props);
+  }
+  getDivList(count: number) {
+    const list = [];
+    for (let i = 0; i < count; i++) {
+      list.push(<div />);
+    }
+    return list;
   }
   render() {
     return (
       <div className="loading-wrapper">
-        <i className="icon-loading" />
-        <div className="hint">{this.props.children || '数据加载中'}</div>
+        {(!this.props.showFlag || this.props.showFlag === 'circle') && (
+          <div className="circle" v-show="showFlag === 1">
+            {this.getDivList(12)}
+          </div>
+        )}
+
+        {this.props.showFlag === 'spinner' && (
+          <div className="spinner" v-show="showFlag === 2">
+            {this.getDivList(12)}
+          </div>
+        )}
+
+        {this.props.showFlag === 'test' && (
+          <div className="test" v-show="showFlag === 3">
+            {this.getDivList(3)}
+          </div>
+        )}
+        <div className="hint">{this.props.children}</div>
       </div>
     );
   }
