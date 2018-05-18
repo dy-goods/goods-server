33819597bc1f858be5694a47f574aba8d4793d65
周新凯 <zhouxinkai@meituan.com>
2018-05-19 00:03:16 +0800

    UPDATE

--- src/types/goods.d.ts
+++ src/types/goods.d.ts
@@ -1,14 +1,14 @@
 declare namespace GOODS {
   interface IGoodsType {
     id: string;
-    videoUrl: string;
+    videoUrl: string; // 视频链接
     stars: number; // 点赞数
     discount: number; // 折扣
     buyCount: number; // 购买数量
-    taobaoPrice: number;
-    price: number; // 以分为单位
-    title: string;
-    imgUrl: string;
+    taobaoPrice: number; // 淘宝价格
+    price: number; // 价格，以分为单位
+    title: string; // 标题
+    imgUrl: string; // 图片链接
     labels: string; // 标签, eg好玩到爆，省事的气球车
     isDeleted: boolean;
     createdAt: number;
